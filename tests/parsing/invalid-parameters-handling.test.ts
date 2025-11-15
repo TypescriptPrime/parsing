8f87e829ba760ce3ebb705c9ad1fab98ace36f02
PiQuark6046 <piquark6046@proton.me>
2025-11-15 10:35:55 +0000

    test: add some spanning across tokens without close

--- tests/parsing/invalid-parameters-handling.test.ts
+++ tests/parsing/invalid-parameters-handling.test.ts
@@ -1,8 +1,31 @@
 import test from 'ava'
 import { Parsing } from '@/index.js'
 
-test('throws error for spanning across tokens without close (inner is double quote)', T => {
-  let Input = ['--parameter1', '"value', 'is', 'wrong"']
+test('throws error for spanning across tokens without close at emd (inner is double quote)', T => {
+  let Input = ['--parameter1', '"value', 'is', 'wrong']
+  T.throws(() => {
+    Parsing(Input)
+  })
+})
+
+test('throws error for spanning across tokens without close at end (inner is single quote)', T => {
+  // eslint-disable-next-line quotes
+  let Input = ['--parameter1', "'value", 'is', "wrong"]
+  T.throws(() => {
+    Parsing(Input)
+  })
+})
+
+test('throws error for spanning across tokens without close at beginning (inner is double quote)', T => {
+  let Input = ['--parameter1', 'value', 'is', 'wrong"']
+  T.throws(() => {
+    Parsing(Input)
+  })
+})
+
+test('throws error for spanning across tokens without close at beginning (inner is single quote)', T => {
+  // eslint-disable-next-line quotes
+  let Input = ['--parameter1', "value", 'is', "wrong'"]
   T.throws(() => {
     Parsing(Input)
   })
