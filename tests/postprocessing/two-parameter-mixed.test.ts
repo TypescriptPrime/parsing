c06f4639be7fe691cf9da62c9913740548906751
PiQuark6046 <piquark6046@proton.me>
2025-11-18 10:13:44 +0000

    test: remove built-in naming convention option as using external package

--- tests/postprocessing/two-parameter-mixed.test.ts
+++ tests/postprocessing/two-parameter-mixed.test.ts
@@ -9,27 +9,3 @@ test('PostProcessing two mixed parameters', T => {
     Parameter2: 'Value2'
   })
 })
-
-test('PostProcessing two mixed parameters with no upper case', T => {
-  let Input = ['--parameter1', 'value1', '--parameter2=value2']
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  let Output = PostProcessing<{ parameter1: string; parameter2: string }>(Input, {
-    UpperStartingCase: false
-  })
-  T.deepEqual(Output, {
-    parameter1: 'value1',
-    parameter2: 'value2'
-  })
-})
-
-test('PostProcessing two mixed parameters with no upper case 2', T => {
-  let Input = ['--Parameter1', 'value1', '--parameter2=value2']
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  let Output = PostProcessing<{ Parameter1: string; parameter2: string }>(Input, {
-    UpperStartingCase: false
-  })
-  T.deepEqual(Output, {
-    Parameter1: 'value1',
-    parameter2: 'value2'
-  })
-})
