d8c11821a9e3f89d80ced31fe59e38e0ffc4c6f3
PiQuark6046 <piquark6046@proton.me>
2025-11-25 12:46:31 +0000

    test: remove test units with equal sign

--- tests/postprocessing/single-parameter.test.ts
+++ tests/postprocessing/single-parameter.test.ts
@@ -9,23 +9,7 @@ test('PostProcessing a single parameter', T => {
   })
 })
 
-test('PostProcessing a single parameter with equal sign', T => {
-  let Input = ['--Parameter1=Value1']
-  let Output = PostProcessing<{ Parameter1: string }>(Input)
-  T.deepEqual(Output, {
-    Parameter1: 'Value1'
-  })
-})
-
 test('PostProcessing a single parameter with no upper case', T => {
-  let Input = ['--parameter1', 'value1']
-  let Output = PostProcessing<{ Parameter1: string }>(Input)
-  T.deepEqual(Output, {
-    Parameter1: 'value1'
-  })
-})
-
-test('PostProcessing a single parameter with no upper case and equal sign', T => {
   let Input = ['--parameter1', 'value1']
   let Output = PostProcessing<{ Parameter1: string }>(Input)
   T.deepEqual(Output, {
--- tests/postprocessing/two-parameter.test.ts
+++ tests/postprocessing/two-parameter.test.ts
@@ -10,25 +10,7 @@ test('PostProcessing a single parameter', T => {
   })
 })
 
-test('PostProcessing a single parameter with equal sign', T => {
-  let Input = ['--Parameter1=Value1', '--Parameter2=Value2']
-  let Output = PostProcessing<{ Parameter1: string, Parameter2: string }>(Input)
-  T.deepEqual(Output, {
-    Parameter1: 'Value1',
-    Parameter2: 'Value2'
-  })
-})
-
 test('PostProcessing a single parameter with no upper case', T => {
-  let Input = ['--parameter1', 'value1', '--parameter2', 'value2']
-  let Output = PostProcessing<{ Parameter1: string, Parameter2: string }>(Input)
-  T.deepEqual(Output, {
-    Parameter1: 'value1',
-    Parameter2: 'value2'
-  })
-})
-
-test('PostProcessing a single parameter with no upper case and equal sign', T => {
   let Input = ['--parameter1', 'value1', '--parameter2', 'value2']
   let Output = PostProcessing<{ Parameter1: string, Parameter2: string }>(Input)
   T.deepEqual(Output, {
