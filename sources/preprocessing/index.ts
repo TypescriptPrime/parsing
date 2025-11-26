bfd7de05f22d9d41597988437918e6fd8c456e23
PiQuark6046 <piquark6046@proton.me>
2025-11-26 16:22:38 +0000

    feat: add PreProcessing to filter process.argv with only options and their values

--- sources/preprocessing/index.ts
+++ sources/preprocessing/index.ts
@@ -1,6 +1,7 @@
 import type * as Process from 'node:process'
 
 type DropFirstANDTwo<T extends readonly unknown[]> = T extends readonly [unknown, unknown, ...infer Tail] ? Tail : []
+type DropFirst<T extends readonly unknown[]> = T extends readonly [unknown, ...infer Tail] ? Tail : []
 
 /**
  * Returns a filtered `process.argv` with only options and their values.
@@ -8,6 +9,10 @@ type DropFirstANDTwo<T extends readonly unknown[]> = T extends readonly [unknown
  * @param {string} Args - A value of `process.argv`.
  * @returns A filtered `process.argv` with only options and their values.
  */
-export function PreProcessing<Args extends typeof Process.argv>(Args: Args): DropFirstANDTwo<Args> {
-  return Args.slice(2) as DropFirstANDTwo<Args>
+export function PreProcessing<Args extends typeof Process.argv>(Args: Args): DropFirstANDTwo<Args> | DropFirst<Args> {
+  if (Args[2].startsWith('--')) {
+    return Args.slice(2) as DropFirstANDTwo<Args>
+  } else {
+    return Args.slice(1) as DropFirst<Args>
+  }
 }
